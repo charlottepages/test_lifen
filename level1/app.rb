8e7e7fa8ff98c05bbc2f707f2c82664184523721
Charlotte Pages <pgs.charlotte@gmail.com>
2019-01-09 23:10:49 +0100

    refacto

--- level1/app.rb
+++ level1/app.rb
@@ -1,26 +1,32 @@
-require "json"
-require "date"
-require "pp"
+require 'json'
+require 'date'
+
+# DOMAIN VARIABLES
+BASE_PRICE = 10
+COLOR_PRICE = 18
+EXTRA_PAGE_PRICE = 7
+EDP_PRICE = 60
 
 # Parse JSON
 filepath = 'data.json'
 serialized_data = File.read(filepath)
 data = JSON.parse(serialized_data, symbolize_names: true)
 
-# Retrieve ids of Practitionners with 'express delivery' true (Array of Integer)
-express_delivery_practitioner_ids = data[:practitioners].select { |p| p[:express_delivery] == true }.map { |p| p[:id] }
+# Retrieve ids of Express Delivery Practitioners (EDP's)
+edps = data[:practitioners].select { |p| p[:express_delivery] }
+edp_ids = edps.map { |p| p[:id] }
 
-# Group Communications by send date (Array of Comms (ruby hashes))
-communication_groups = data[:communications].group_by { |c| Date.parse(c[:sent_at]).to_s }
+# Group Communications by send date
+comm_groups = data[:communications].group_by { |c| Date.parse(c[:sent_at]).to_s }
 
-# Calculate total for each group and build output
+# Calculate total for each group of Communications and build output
 totals = []
-communication_groups.each do |day, comms|
+comm_groups.each do |day, comms|
   day_comm_values = comms.map do |comm|
-    comm_value_cents = 10
-    comm_value_cents += 18 if comm[:color]
-    comm_value_cents += (comm[:pages_number] - 1) * 7
-    comm_value_cents += 60 if express_delivery_practitioner_ids.include?(comm[:practitioner_id])
+    comm_value_cents = BASE_PRICE
+    comm_value_cents += COLOR_PRICE if comm[:color]
+    comm_value_cents += (comm[:pages_number] - 1) * EXTRA_PAGE_PRICE
+    comm_value_cents += EDP_PRICE if edp_ids.include?(comm[:practitioner_id])
     comm_value_cents
   end
   totals.push(
